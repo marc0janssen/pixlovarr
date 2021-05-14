f64a60f49d29cff1a6af4b2f06544eb49cd8197c
Marco <5788988+marc0janssen@users.noreply.github.com>
2021-05-14 22:39:49 +0200

    added push script

---
+++ push.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Name: Pixlovarr
+# Coder: Marco Janssen (twitter @marc0janssen)
+# date: 2021-05-14 22:39:24
+# update: 2021-05-14 22:39:30
+
+docker push marc0janssen/pixlovarr:latest
+docker pushrm marc0janssen/pixlovarr:latest
