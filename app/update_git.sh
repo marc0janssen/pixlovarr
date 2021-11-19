6a9ae9ee7b558d0ffe041768a4c0d0c7529b657d
Marco <5788988+marc0janssen@users.noreply.github.com>
2021-11-19 18:33:20 +0100

    start.sh

--- app/update_git.sh
+++ app/update_git.sh
@@ -9,6 +9,7 @@ cp /tmp/pixlovarr/app/* /app
 cp /tmp/radarr-library-purge/app/* /app
 
 chmod +x /app/pixlovarr.py
+chmod +x /app/start.sh
 chmod +x /app/update_git.sh
 chmod +x /app/radarr_library_purge.py
 
