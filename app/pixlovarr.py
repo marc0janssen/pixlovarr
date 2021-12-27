4d3ba03f461bf81b70443b79723ae363dba6624b
Marco <5788988+marc0janssen@users.noreply.github.com>
2021-12-27 14:00:35 +0100

    changed startup

--- app/pixlovarr.py
+++ app/pixlovarr.py
@@ -46,7 +46,7 @@ class Pixlovarr():
 
     def __init__(self):
 
-        self.version = "1.17.5.2435"
+        self.version = "1.17.5.2438"
         self.startTime = datetime.now()
         config_dir = "./config"
         app_dir = "./app"
--- app/start.sh
+++ app/start.sh
@@ -6,12 +6,12 @@
 # update: 2021-11-25 21:31:52
 
 echo "Set Crons for Pixlovarr"
-echo '${PRUNE_CRON} python3 /app/pixlovarr_prune.py &> /proc/1/fd/1 2>&1' >> /etc/crontabs/root
+echo "${PRUNE_CRON} python3 /app/pixlovarr_prune.py &> /proc/1/fd/1 2>&1" >> /etc/crontabs/root
 
-echo"Start the cron process"
+echo "Start the cron process"
 nohup crond -l 2 -f &
 
-echo"Start the main process"
+echo "Start the main process"
 nohup /usr/bin/python3 /app/pixlovarr.py &
   
 # Wait for any process to exit
