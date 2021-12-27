6103426f36181c0b4ad26a40985102dff4901b8c
Marco <5788988+marc0janssen@users.noreply.github.com>
2021-12-27 13:56:36 +0100

    changed startup

--- app/start.sh
+++ app/start.sh
@@ -5,13 +5,13 @@
 # date: 2021-11-19 18:53:35
 # update: 2021-11-25 21:31:52
 
-# Set Crons for Pixlovarr
+echo "Set Crons for Pixlovarr"
 echo '${PRUNE_CRON} python3 /app/pixlovarr_prune.py &> /proc/1/fd/1 2>&1' >> /etc/crontabs/root
 
-# Start the cron process
+echo"Start the cron process"
 nohup crond -l 2 -f &
 
-# Start the main process
+echo"Start the main process"
 nohup /usr/bin/python3 /app/pixlovarr.py &
   
 # Wait for any process to exit
--- scripts/start_pixlovarr.sh
+++ scripts/start_pixlovarr.sh
@@ -3,13 +3,14 @@
 # Name: Pixlovarr
 # Coder: Marco Janssen (twitter @marc0janssen)
 # date: 2021-04-21 20:23:43
-# update: 2021-12-27 13:43:41
+# update: 2021-12-27 13:49:04
 
 docker run -d \
 	--name=pixlovarr \
 	--restart=always \
+	-v /volume1/docker/pixlovarr/log:/log \
 	-v /volume1/docker/pixlovarr/config:/config \
 	-v /volume1/video/movies:/movies \
 	-v /volume2/video2/movies:/movies2 \
-	-e PRUNE_CRON="* 4,* * *" \
+	-e PRUNE_CRON="* 4,16 * * *" \
 	marc0janssen/pixlovarr:latest
