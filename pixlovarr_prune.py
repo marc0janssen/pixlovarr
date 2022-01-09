224aadd62d83499f83cb5accc1662bf735e3d98d
Marco <5788988+marc0janssen@users.noreply.github.com>
2022-01-09 14:11:10 +0100

    log

--- pixlovarr_prune.py
+++ pixlovarr_prune.py
@@ -494,7 +494,7 @@ class RLP():
 
             logfile = open(self.log_filePath, "r")
 
-            body += str(logfile.readlines())
+            body += ''.join(logfile.readlines())
 
             logfile.close()
 
